3dd7c806d7954b5bf5b1f1ecb6812e3797326e13
JacobBeckfeld <jacbeck456@gmail.com>
2021-07-09 17:27:10 -0500

    added an init function that allows you to choose if you want to continue to add employees

--- index.js
+++ index.js
@@ -5,33 +5,134 @@ const Intern = require("./lib/Intern");
 const fs = require("fs");
 
 
+// const questions = [
+//     {
+//         type: "input",
+//         message: "What is you employee's name?",
+//         name: "name"
+//     },
+//     {
+//         type: "input",
+//         message: "What is their employee id?",
+//         name: "id"
+//     },
+//     {
+//         message: "Please enter your email",
+//         name: "email",
+//         type: "input",
+//         default: () => {},
+//         validate: function (email) {
+
+//             valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+//             if (valid) {
+//                 console.log("Great job");
+//                 return true;
+//             } else {
+//                 console.log(".  Please enter a valid email")
+//                 return false;
+//             }
+//         }
+//     },
+
+
+// ]
+
+// const managerQuestion = {
+//     type:"input",
+//     message: "What is the employee's office number?",
+//     name: "number"
+// }
 
 
 
+// const getName = () => {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "What is you employee's name?",
+//             name: "name"
+//         },
+//     ])
+// }
 
+// const getId = () => {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "What is their employee id?",
+//             name: "id"
+//         },
+//     ])
+// }
 
-const getName = () => {
+// const getEmail = () => {
+//     inquirer.prompt([
+//         {
+//             message: "Please enter your email",
+//             name: "email",
+//             type: "input",
+//             default: () => {},
+//             validate: function (email) {
+    
+//                 valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+//                 if (valid) {
+//                     console.log("Great job");
+//                     return true;
+//                 } else {
+//                     console.log(".  Please enter a valid email")
+//                     return false;
+//                 }
+//             }
+//         },
+//     ])
+// }
+
+let empArray = [];
+
+// const getGithub = () => {
+//     inquirer.prompt([
+//         {
+//             type:"input",
+//             message: "What is the employee's github?",
+//             name:"github"
+//         },
+//     ])
+// }
+
+// const getNumber = () => {
+//     inquirer.prompt([
+//         {
+//             type:"input",
+//             message: "What is the employee's office number?",
+//             name: "number"
+//         },
+//     ])
+// }
+
+// const getSchool = () => {
+//     inquirer.prompt([
+//         {
+//             type:"input",
+//             message: "Where does this employee go to school?",
+//             name: "school"
+//         },
+//     ])
+// }
+
+const addManager = () =>{
     inquirer.prompt([
         {
             type: "input",
             message: "What is you employee's name?",
             name: "name"
         },
-    ])
-}
-
-const getId = () => {
-    inquirer.prompt([
         {
             type: "input",
             message: "What is their employee id?",
             name: "id"
         },
-    ])
-}
-
-const getEmail = () => {
-    inquirer.prompt([
         {
             message: "Please enter your email",
             name: "email",
@@ -50,52 +151,112 @@ const getEmail = () => {
                 }
             }
         },
-    ])
-}
-
-const getGithub = () => {
-    inquirer.prompt([
-        {
-            type:"input",
-            message: "What is the employee's github?",
-            name:"github"
-        },
-    ])
-}
-
-const getNumber = () => {
-    inquirer.prompt([
         {
             type:"input",
             message: "What is the employee's office number?",
             name: "number"
         },
-    ])
-}
+    
+    ]).then(data => {
+        let {name, id, email, number} = data
+        let manager = new Manager(name, id, email, number)
 
-const getSchool = () => {
+        empArray.push(manager);
+        console.log(manager);
+        init();
+    })
+
+    
+};
+
+const addEngineer = () => {
     inquirer.prompt([
+        {
+            type: "input",
+            message: "What is you employee's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is their employee id?",
+            name: "id"
+        },
+        {
+            message: "Please enter your email",
+            name: "email",
+            type: "input",
+            default: () => {},
+            validate: function (email) {
+    
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+                if (valid) {
+                    console.log("Great job");
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
+        },
+        {
+            type:"input",
+            message: "What is the employee's github?",
+            name:"github"
+        },
+
+    ]).then(data => {
+        const {name, id, email, github} = data
+        const engineer = new Engineer(name, id, email, github)
+        empArray.push(engineer)
+        init()
+    })
+};
+
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is you employee's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is their employee id?",
+            name: "id"
+        },
+        {
+            message: "Please enter your email",
+            name: "email",
+            type: "input",
+            default: () => {},
+            validate: function (email) {
+    
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+                if (valid) {
+                    console.log("Great job");
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
+        },
         {
             type:"input",
             message: "Where does this employee go to school?",
             name: "school"
         },
-    ])
-}
 
-const addManager = () =>{
-    getName()
+    ]).then( data => {
+        const {name, id, email, school} = data;
+        const intern = new Intern(name, id, email, school);
+        empArray.push(intern)
+        init();
+    })
 
-    
-}
-
-const addEngineer = () => {
-
-}
-
-const addIntern = () => {
-
-}
+};
 
 const getRole = () => {
     inquirer.prompt([
@@ -122,10 +283,34 @@ const getRole = () => {
             default:
                 break;
         }
+        return empArray
     })
 
-}
-addManager();
-// getRole();
+};
+
+const init = () =>{
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "Would you like to add another employee",
+            choices: ["Yes", "No"],
+            name: "initialChoice"
+        }
+    ).then( resp  => {
+        switch(resp.initialChoice){
+            case "Yes":
+                getRole();
+                break;
+
+            case "No":
+                break;
+        }
+    })
+};
 
 
+
+init();
+// getRole(console.log(empArray));
+// addManager();
+// console.log(empArray)
